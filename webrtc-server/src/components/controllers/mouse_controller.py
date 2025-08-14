f3c6b52bd0b1131841314bde58fe4ff6d31db015
JustNevi <slavkad25@gmail.com>
2025-08-14 12:18:44 +0200

    feat(rtc-server): limit mouse in gui

--- webrtc-server/src/components/controllers/mouse_controller.py
+++ webrtc-server/src/components/controllers/mouse_controller.py
@@ -5,6 +5,19 @@ class MouseController:
         self.is_left_down = False
         self.is_right_down = False
 
+        x, y = gui.size()
+        self.size = (x, y) 
+
+    def in_gui(func):
+        def wrapper(self, button, x, y, *args, **kwargs):
+            xg = x >=0 and x < self.size[0]
+            yg = y >= 0 and y < self.size[1]
+            if (xg and yg):
+                func(self, button, x, y, *args, **kwargs)
+
+        return wrapper
+
+    @in_gui
     def down(self, button, x, y):
         if (button == "left"):
             if (self.is_left_down):
@@ -17,6 +30,7 @@ class MouseController:
 
         gui.mouseDown(x=x, y=y, button=button)
 
+    @in_gui
     def up(self, button, x, y):
         if (button == "left"):
             self.is_left_down = False 
