a75dcaadf69fcd3849b0350a3f002ee3d9d432aa
JustNevi <slavkad25@gmail.com>
2025-08-16 16:24:46 +0200

    feat(rtc-server): capture screen in seperate tread and recieve via queue

--- webrtc-server/src/components/rtc/tracks/screen_capture_track.py
+++ webrtc-server/src/components/rtc/tracks/screen_capture_track.py
@@ -1,5 +1,7 @@
 import asyncio
 import time
+import threading
+import queue
 import cv2
 import mss
 import numpy as np
@@ -14,34 +16,53 @@ class ScreenCaptureTrack(VideoStreamTrack):
     """
     def __init__(self, fps=30):
         super().__init__()
-        self.sct = mss.mss() # Initialize mss for screen capture
-        self.monitor = self.sct.monitors[1] # Capture the primary monitor
+        self.monitor = None 
         self.fps = fps
 
+
+        self.running = True
+        self.queue = queue.Queue(maxsize=4)
+        self.thread = threading.Thread(target=self.capture, daemon=True)
+
+        self.start()
+
+    def start(self):
+        self.thread.start()
+
+    def capture(self):
+        with mss.mss() as sct:
+            self.monitor = sct.monitors[1]
+            while (self.running):
+                """
+                Capture a frame from the screen and return it as a VideoFrame.
+                """
+                # Get a screen shot
+                screenshot = sct.grab(self.monitor)
+
+                # Convert screenshot to a NumPy array
+                img = np.array(screenshot)
+
+                # Convert the image from BGRA to BGR
+                img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+                
+                # Create a PyAV VideoFrame from the NumPy array
+                frame = VideoFrame.from_ndarray(img, format="bgr24")
+                
+                # Set the timestamp for the frame
+                pts = int(time.time() * 1000)
+                frame.pts = pts
+                
+                # Set the time base (e.g., 1/1000)
+                frame.time_base = Fraction(1, 1000)
+                
+                # Empty all old frames 
+                while (not self.queue.empty()):
+                    self.queue.get_nowait()
+                
+                self.queue.put_nowait(frame)
+
+                # Control the frame rate
+                time.sleep(1 / self.fps)
+
     async def recv(self):
-        """
-        Capture a frame from the screen and return it as a VideoFrame.
-        """
-        # Get a screen shot
-        screenshot = self.sct.grab(self.monitor)
-
-        # Convert screenshot to a NumPy array
-        img = np.array(screenshot)
-
-        # Convert the image from BGRA to BGR
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
-        
-        # Create a PyAV VideoFrame from the NumPy array
-        frame = VideoFrame.from_ndarray(img, format="bgr24")
-        
-        # Set the timestamp for the frame
-        pts = int(time.time() * 1000)
-        frame.pts = pts
-        
-        # Set the time base (e.g., 1/1000)
-        frame.time_base = Fraction(1, 1000) # <-- Corrected line
-
-        # Wait for the next frame
-        await asyncio.sleep(1 / self.fps)  # Adjust this for desired frame rate (e.g., 30 FPS)
-        
-        return frame
+        return self.queue.get() 
