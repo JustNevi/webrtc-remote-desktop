ea79cb6118019e7d27e22d7ec027018dd015cb5f
JustNevi <slavkad25@gmail.com>
2025-08-17 19:30:05 +0200

    chore(rtc-server): make docker container with virtual display

---
+++ webrtc-server/docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Explicitly tell Xlib not to use the Xauthority file
+export XAUTHORITY=/dev/null
+
+# Start Xvfb in the background
+Xvfb :99 -screen 0 1024x768x24 -ac &
+
+# Wait for Xvfb to be ready
+sleep 2
+
+# Set the display environment variable
+export DISPLAY=:99
+
+# Start Fluxbox (the window manager) in the background
+fluxbox &
+
+# Start xterm (the terminal emulator) and your Quart app
+exec hypercorn --bind 0.0.0.0:8000 main:app &
+exec xterm -geometry 80x24+10+10 &
+
+# Keep the script running to prevent the container from exiting
+tail -f /dev/null
