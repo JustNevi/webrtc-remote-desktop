4b6b232f2be195efbea0b4861f2a7a741faef927
JustNevi <slavkad25@gmail.com>
2025-08-19 02:39:42 +0200

    feat(rtc-client): close peer connection when windows closes

--- webrtc-client/src/main.py
+++ webrtc-client/src/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import json
 import queue
 import threading
@@ -23,9 +24,10 @@ SERVER_PORT = os.getenv("SERVER_PORT")
 SERVER_ENDPOINT = f"http://{SERVER_IP}:{SERVER_PORT}"
 
 
-async def run_rtc(frame_queue, control_queue):
+async def run_rtc(frame_queue, control_queue, close_event):
     # Create rtc api
     rtc = RTCApi(is_offer=True, frame_queue=frame_queue, control_queue=control_queue) 
+    rtc.init_peer_connection()
 
     sdp = await rtc.createSession()
 
@@ -38,23 +40,27 @@ async def run_rtc(frame_queue, control_queue):
     await rtc.acceptAnswer(sdp)
 
     try:
-        while True:
-            await asyncio.sleep(1)
-    except KeyboardInterrupt:
-        pass
+        await close_event.wait()
     finally:
         await rtc.shutdown()
+        logging.info("RTC shutdown complete.")
 
-def start_rtc(frame_queue, control_queue):
+def start_rtc(frame_queue, control_queue, close_event):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
     def target():
-        asyncio.run(run_rtc(frame_queue, control_queue))
+        loop.run_until_complete(run_rtc(frame_queue, control_queue, close_event))
 
     rtc_thread = threading.Thread(target=target, daemon=True)
     rtc_thread.start()
+    return loop
 
 def start_gui(frame_queue, control_input):
     gui = Dearpygui("MainView", 1320, 840, frame_queue, control_input)
+    # The program stops here until the window is closed.
     gui.init_gui()
+
 
 def main():
     # Create queue of frames to deliver between RTC and GUI
@@ -67,8 +73,17 @@ def main():
 
     input = Input(on_input)
 
-    start_rtc(frame_queue, control_queue)
+    close_event = asyncio.Event()
+
+    # === Start ===
+    loop = start_rtc(frame_queue, control_queue, close_event)
     start_gui(frame_queue, input)
+
+    # === End ===
+    # Runs when windows are closed 
+    loop.call_soon_threadsafe(close_event.set)
+    # Wait for rtc peer connection closes
+    time.sleep(1)
 
 
 if ( __name__ == "__main__"):
